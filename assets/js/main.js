60df5331c2bef6ca25b7246dcb31aa6796f258b7
IYAOAA <ibrahimabiodunyahaya@gmail.com>
2025-09-21 17:04:42 +0000

    Quick deploy update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 // --- SET YOUR SUPABASE CREDENTIALS ---
-const supabaseUrl = 'https://YOUR_PROJECT_ID.supabase.co';
-const supabaseKey = 'YOUR_PUBLIC_ANON_KEY';
+const supabaseUrl = 'https://sqgnjqgsxeamvyciyezb.supabase.co';
+const supabaseKey = 'sb_secret_KDaURoOKuCSSweJim3PIsQ_FbxGjTHA';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 let allPosts = [];
